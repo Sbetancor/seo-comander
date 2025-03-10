bb810006c99f8b175b44607b293241824c402dd0
sabele90 <sabele@hotmail.es>
2025-03-10 16:14:43 +0000

    plantillas

--- vristo-vue-starter/src/views/clients.vue
+++ vristo-vue-starter/src/views/clients.vue
@@ -33,12 +33,7 @@
                             <div class="font-semibold">{{ data.value.firstName + ' ' + data.value.lastName }}</div>
                         </div>
                     </template>
-                    <template #country>
-                        <div class="flex items-center gap-2">
-                            <img width="24" :src="`/assets/images/flags/${getCountry().code}.svg`" class="max-w-none" alt="user profile" />
-                            <div>{{ getCountry().name }}</div>
-                        </div>
-                    </template>
+               
                     <template #email="data">
                         <a :href="`mailto:${data.value.email}`" class="text-primary hover:underline">{{ data.value.email }}</a>
                     </template>
@@ -95,7 +90,6 @@
     const cols = ref([
         { field: 'id', title: 'ID', isUnique: true },
         { field: 'firstName', title: 'User' },
-        { field: 'country', title: 'Country', sort: false },
         { field: 'email', title: 'Email' },
         { field: 'age', title: 'Progress', sort: false },
         { field: 'phone', title: 'Phone' },
@@ -606,90 +600,6 @@
         },
     ]);
 
-    const countryList = [
-        { code: 'AE', name: 'United Arab Emirates' },
-        { code: 'AR', name: 'Argentina' },
-        { code: 'AT', name: 'Austria' },
-        { code: 'AU', name: 'Australia' },
-        { code: 'BE', name: 'Belgium' },
-        { code: 'BG', name: 'Bulgaria' },
-        { code: 'BN', name: 'Brunei' },
-        { code: 'BR', name: 'Brazil' },
-        { code: 'BY', name: 'Belarus' },
-        { code: 'CA', name: 'Canada' },
-        { code: 'CH', name: 'Switzerland' },
-        { code: 'CL', name: 'Chile' },
-        { code: 'CN', name: 'China' },
-        { code: 'CO', name: 'Colombia' },
-        { code: 'CZ', name: 'Czech Republic' },
-        { code: 'DE', name: 'Germany' },
-        { code: 'DK', name: 'Denmark' },
-        { code: 'DZ', name: 'Algeria' },
-        { code: 'EC', name: 'Ecuador' },
-        { code: 'EG', name: 'Egypt' },
-        { code: 'ES', name: 'Spain' },
-        { code: 'FI', name: 'Finland' },
-        { code: 'FR', name: 'France' },
-        { code: 'GB', name: 'United Kingdom' },
-        { code: 'GR', name: 'Greece' },
-        { code: 'HK', name: 'Hong Kong' },
-        { code: 'HR', name: 'Croatia' },
-        { code: 'HU', name: 'Hungary' },
-        { code: 'ID', name: 'Indonesia' },
-        { code: 'IE', name: 'Ireland' },
-        { code: 'IL', name: 'Israel' },
-        { code: 'IN', name: 'India' },
-        { code: 'IT', name: 'Italy' },
-        { code: 'JO', name: 'Jordan' },
-        { code: 'JP', name: 'Japan' },
-        { code: 'KE', name: 'Kenya' },
-        { code: 'KH', name: 'Cambodia' },
-        { code: 'KR', name: 'South Korea' },
-        { code: 'KZ', name: 'Kazakhstan' },
-        { code: 'LA', name: 'Laos' },
-        { code: 'LK', name: 'Sri Lanka' },
-        { code: 'MA', name: 'Morocco' },
-        { code: 'MM', name: 'Myanmar' },
-        { code: 'MO', name: 'Macau' },
-        { code: 'MX', name: 'Mexico' },
-        { code: 'MY', name: 'Malaysia' },
-        { code: 'NG', name: 'Nigeria' },
-        { code: 'NL', name: 'Netherlands' },
-        { code: 'NO', name: 'Norway' },
-        { code: 'NZ', name: 'New Zealand' },
-        { code: 'PE', name: 'Peru' },
-        { code: 'PH', name: 'Philippines' },
-        { code: 'PK', name: 'Pakistan' },
-        { code: 'PL', name: 'Poland' },
-        { code: 'PT', name: 'Portugal' },
-        { code: 'QA', name: 'Qatar' },
-        { code: 'RO', name: 'Romania' },
-        { code: 'RS', name: 'Serbia' },
-        { code: 'RU', name: 'Russia' },
-        { code: 'SA', name: 'Saudi Arabia' },
-        { code: 'SE', name: 'Sweden' },
-        { code: 'SG', name: 'Singapore' },
-        { code: 'SK', name: 'Slovakia' },
-        { code: 'TH', name: 'Thailand' },
-        { code: 'TN', name: 'Tunisia' },
-        { code: 'TR', name: 'Turkey' },
-        { code: 'TW', name: 'Taiwan' },
-        { code: 'UK', name: 'Ukraine' },
-        { code: 'UG', name: 'Uganda' },
-        { code: 'US', name: 'United States' },
-        { code: 'VN', name: 'Vietnam' },
-        { code: 'ZA', name: 'South Africa' },
-        { code: 'BA', name: 'Bosnia and Herzegovina' },
-        { code: 'BD', name: 'Bangladesh' },
-        { code: 'EE', name: 'Estonia' },
-        { code: 'IQ', name: 'Iraq' },
-        { code: 'LU', name: 'Luxembourg' },
-        { code: 'LV', name: 'Latvia' },
-        { code: 'MK', name: 'North Macedonia' },
-        { code: 'SI', name: 'Slovenia' },
-        { code: 'PA', name: 'Panama' },
-    ];
-
     const chart_options = computed(() => {
         let option = {
             chart: { sparkline: { enabled: true } },
@@ -732,8 +642,5 @@
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
-    const getCountry = () => {
-        const random = Math.floor(Math.random() * countryList.length);
-        return countryList[random];
-    };
+  
 </script>
